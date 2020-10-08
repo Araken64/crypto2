3d9e303946abfeef03bfea15e511d339234f902e
Araken64 <elian.araken@gmail.com>
2020-10-08 20:09:01 +0200

    First Solution

--- Peer.java
+++ Peer.java
@@ -1,10 +1,20 @@
+import java.util.Arrays;
+
 public class Peer {
     protected String _name;
     protected Peer   _friend;
+    private java.security.KeyPair _keypair;
     
+    public Peer(String name, java.security.KeyPair keypair) {
+        _name   = name;
+	    _friend = null;
+	    _keypair = keypair;
+    }
+
     public Peer(String name) {
         _name   = name;
-	_friend = null;
+        _friend = null;
+        _keypair = null;
     }
     
     private void connect(Peer p) {
@@ -21,11 +31,37 @@ public class Peer {
     }
     
     public void send(byte[] msg) {
-        System.out.println("["+_name+"] \""+new String(msg)+"\" ->");
-        _friend.receive(msg);
+        String strMsg = new String(msg);
+        System.out.println("["+_name+"] \""+ strMsg +"\" ->");
+        _friend.receive(crypter(strMsg));
     }
     
     public void receive(byte[] msg) {
-        System.out.println("["+_name+"] -> \""+new String(msg)+"\"");
+        String stringMsg = decrypter(msg);
+        System.out.println("["+_name+"] -> \""+ stringMsg +"\"");
+    }
+
+    public byte[] crypter(String msg) {
+        try {
+            javax.crypto.Cipher enc = javax.crypto.Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            enc.init(javax.crypto.Cipher.ENCRYPT_MODE, _keypair.getPrivate());
+            enc.update(msg.getBytes());
+            return enc.doFinal();
+        } catch (java.security.NoSuchAlgorithmException | java.security.InvalidKeyException | javax.crypto.IllegalBlockSizeException | javax.crypto.BadPaddingException | javax.crypto.NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+        return msg.getBytes();
+    }
+
+    private String decrypter(byte[] buffer) {
+        try {
+            javax.crypto.Cipher dec = javax.crypto.Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            dec.init(javax.crypto.Cipher.DECRYPT_MODE, _keypair.getPublic());
+            dec.update(buffer);
+            return (new String(dec.doFinal()));
+        } catch (java.security.NoSuchAlgorithmException | javax.crypto.NoSuchPaddingException | java.security.InvalidKeyException | javax.crypto.IllegalBlockSizeException | javax.crypto.BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return Arrays.toString(buffer);
     }
 }
--- Program02.java
+++ Program02.java
@@ -1,11 +1,27 @@
+import com.sun.istack.internal.Nullable;
+
 public class Program02 {
     public static void main(String[] args) {
-	Peer anna = new Peer("anna");
-	Peer bob  = new Peer("bob");
-	//anna.bind(bob);
-	anna.bind(new Hacker(bob));
+		java.security.KeyPair pair = generateKeys();
+		Peer anna = new Peer("anna", pair);
+		Peer bob  = new Peer("bob", pair);
+		//anna.bind(bob);
+		anna.bind(new Hacker(bob));
 
-	anna.send("coucou");
-	bob.send("hello");
+		anna.send("coucou");
+		bob.send("hello");
     }
+
+    private static @Nullable java.security.KeyPair generateKeys() {
+    	try {
+			java.security.SecureRandom random = java.security.SecureRandom.getInstance("SHA1PRNG");
+			java.security.KeyPairGenerator keyGen = java.security.KeyPairGenerator.getInstance("RSA");
+			int bitsize = 1024;
+			keyGen.initialize(1024, random);
+			return (keyGen.generateKeyPair());
+		} catch (java.security.NoSuchAlgorithmException e) {
+    		e.printStackTrace();
+		}
+    	return null;
+	}
 }
